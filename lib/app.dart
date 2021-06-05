e3ffe6b9e7484708102cffe4460034e3ab4357be
imjeen <imjeen@mail.com>
2021-06-05 19:21:35 +0800

    init

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:photo_studio/routes/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:photo_studio/app.dart';
+
+void main() {
+  runApp(App());
+}
